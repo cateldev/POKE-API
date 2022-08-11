c4490f3a627f913f8ee6720447e40c72f52c3e1f
Matheus Catel <catelmatt@hotmail.com>
2022-08-11 15:27:33 -0300

    Pokemon List  Script Added

--- POKE-API/pokedex/src/App.js
+++ POKE-API/pokedex/src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getPokemons } from "./api";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Pokedex from "./components/Pokedex";
 import Searchbar from "./components/Searchbar";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true);
+      const result = await getPokemons();
+      setPokemons(result);
+      setLoading(false);
+    } catch (error) {
+      console.log("fetchPokemons error: ", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("carregou");
+    fetchPokemons();
+  });
 
   return (
     <div>
       <Navbar />
       <Searchbar />
+      <Pokedex pokemons={pokemons} loading={loading} />
       <div className="App"></div>
     </div>
   );
