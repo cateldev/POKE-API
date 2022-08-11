4860253f3bd63b808c7f4b843b9263ebe76a885a
Matheus Catel <catelmatt@hotmail.com>
2022-08-11 13:21:16 -0300

    Fixes

--- POKE-API/pokedex/src/App.js
+++ POKE-API/pokedex/src/App.js
@@ -1,8 +1,10 @@
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Searchbar from "./components/Searchbar";
 
 function App() {
+
   return (
     <div>
       <Navbar />
