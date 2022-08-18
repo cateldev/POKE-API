fbe7fa1fecf8f84fe9699798ac7bfc12ffebcf58
Matheus Catel <catelmatt@hotmail.com>
2022-08-18 17:19:53 -0300

    setPages Added

--- POKE-API/pokedex/src/App.js
+++ POKE-API/pokedex/src/App.js
@@ -6,13 +6,16 @@ import Pokedex from "./components/Pokedex/Pokedex.js";
 import Searchbar from "./components/Searchbar/Searchbar.js";
 
 function App() {
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
   const [pokemons, setPokemons] = useState([]);
 
+  const itemsPerPage = 24;
   const fetchPokemons = async () => {
     try {
       setLoading(true);
-      const data = await getPokemons();
+      const data = await getPokemons(itemsPerPage, itemsPerPage * page);
       const promises = data.results.map(async (pokemon) => {
         return await getPokemonData(pokemon.url);
       });
@@ -20,6 +23,7 @@ function App() {
       const results = await Promise.all(promises);
       setPokemons(results);
       setLoading(false);
+      setTotalPages(Math.ceil(data.count / itemsPerPage));
     } catch (error) {
       console.log("fetchPokemons error: ", error);
     }
@@ -28,13 +32,19 @@ function App() {
   useEffect(() => {
     console.log("carregou");
     fetchPokemons();
-  }, []);
+  }, [page]);
 
   return (
     <div>
       <Navbar />
       <Searchbar />
-      <Pokedex pokemons={pokemons} loading={loading} />
+      <Pokedex
+        pokemons={pokemons}
+        loading={loading}
+        page={page}
+        setPage={setPage}
+        totalPages={totalPages}
+      />
       <div className="App"></div>
     </div>
   );
