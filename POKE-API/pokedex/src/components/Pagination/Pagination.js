8509a5368bdbceae55076afb78fe005ffc935946
Matheus Catel <catelmatt@hotmail.com>
2022-08-19 11:22:57 -0300

    Pagination Component Update

--- POKE-API/pokedex/src/components/Pagination/Pagination.js
+++ POKE-API/pokedex/src/components/Pagination/Pagination.js
@@ -1,15 +1,20 @@
 import React from "react";
+import "./Pagination.css";
 
 const Pagination = (props) => {
-const { page, totalPages, onLeftClick, onRightClick } = props;
+  const { page, totalPages, onLeftClick, onRightClick } = props;
 
   return (
     <div className="pagination-container">
-      <button onClick={onLeftClick}><div>◀️</div></button>
+      <button onClick={onLeftClick}>
+        <div>◀️</div>
+      </button>
       <div>
         {page} of {totalPages}
       </div>
-      <button onClick={onRightClick}><div>▶️</div></button>
+      <button onClick={onRightClick}>
+        <div>▶️</div>
+      </button>
     </div>
   );
 };
